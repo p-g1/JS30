3d2872e00d6ae77fd337506de1c7a1576ca95710
Pete <petergarvin1@gmail.com>
2017-01-22 19:45:21 +0000

    half way

---
+++ Custom_Vid_Player/scripts.js
@@ -0,0 +1,27 @@
+/*  get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+
+/* build functions */
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+
+/* hook up listeners */
+video.addEventListener('play', togglePlay);
+video.addEventListener('click', togglePlay);
+
+
+toggle.addEventListener('click', togglePlay);
